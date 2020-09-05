3f8e74733dd0dc88282fb3afa99b3809bf433dd0
اسماعیل زارع <zarehesmaiel@gmail.com>
2020-09-05 14:29:32 +0430

    Update FallbackController.php

--- src/HttpSite/Http/Controllers/FallbackController.php
+++ src/HttpSite/Http/Controllers/FallbackController.php
@@ -1,6 +1,7 @@
 <?php
 namespace Core\HttpSite\Http\Controllers; 
   
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Core\HttpSite\Http\Requests\FallbackRequest;  
 use Core\HttpSite\Contracts\SiteRequest;
 use Core\Document\Document;
@@ -8,7 +9,8 @@ use Core\Document\Document;
 class FallbackController extends SiteController
 {      
     public function getContent(SiteRequest $request, Document $document)
-    {    
-    	throw new \Exception('Route Mismatched...!', 404);
+    {     
+    	return 'The requested URL [URL] was not found on this server';
+    	throw new NotFoundHttpException('The requested URL [URL] was not found on this server');
     }
 }
