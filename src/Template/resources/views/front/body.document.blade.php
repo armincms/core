f3bdee38c5996ecb1a753b10df23213d1685adbf
اسماعیل زارع <zarehesmaiel@gmail.com>
2021-08-09 17:37:19 +0430

    Stops loading view when modules is empty

--- src/Template/resources/views/front/body.document.blade.php
+++ src/Template/resources/views/front/body.document.blade.php
@@ -1,49 +1,59 @@
 <div class="body">
 	@for ($i = 1; $i < 4; $i++)
 		@var($section = 'header'. $i) 
-		@component('template::front.section', compact('section')) 
-			@var($width = $this->containerWidth($section))
-			@component('template::front.container', compact('section', 'width')) 
-				{!! $this->renderedModules($section) !!}
+		@if($renderedModules = $this->renderedModules($section))
+			@component('template::front.section', compact('section')) 
+				@var($width = $this->containerWidth($section))
+				@component('template::front.container', compact('section', 'width')) 
+					{!! $renderedModules !!}
+				@endcomponent
 			@endcomponent
-		@endcomponent
+		@endif
 	@endfor
 
 	@component('template::front.section', ['section' => 'menu'])
 		@var($section = 'menu')
 		@var($width = $this->containerWidth($section))
-		@component('template::front.container', compact('section', 'width')) 
-			{!! $this->renderedModules($section) !!}
-		@endcomponent
+		@if($renderedModules = $this->renderedModules($section))
+			@component('template::front.container', compact('section', 'width')) 
+				{!! $this->renderedModules($section) !!}
+			@endcomponent
+		@endif
 	@endcomponent
 
 	@for ($i = 1; $i < 11; $i++)
 		@var($section = 'top'. $i) 
-		@component('template::front.section', compact('section')) 
-			@var($width = $this->containerWidth($section))
-			@component('template::front.container', compact('section', 'width')) 
-				{!! $this->renderedModules($section) !!}
+		@if($renderedModules = $this->renderedModules($section)) 
+			@component('template::front.section', compact('section')) 
+				@var($width = $this->containerWidth($section))
+				@component('template::front.container', compact('section', 'width')) 
+					{!! $this->renderedModules($section) !!}
+				@endcomponent
 			@endcomponent
-		@endcomponent
+		@endif
 	@endfor
 
 	@include('template::front.middle')
 
 	@for ($i = 1; $i < 11; $i++)
 		@var($section = 'footer'. $i)
+		@if($renderedModules = $this->renderedModules($section))
+			@component('template::front.section', compact('section'))
+				@var($width = $this->containerWidth($section))
+				@component('template::front.container', compact('section', 'width')) 
+					{!! $this->renderedModules($section) !!}
+				@endcomponent
+			@endcomponent
+		@endif
+	@endfor
+
+	@var($section = 'copyright') 
+	@if($renderedModules = $this->renderedModules($section))
 		@component('template::front.section', compact('section'))
 			@var($width = $this->containerWidth($section))
 			@component('template::front.container', compact('section', 'width')) 
 				{!! $this->renderedModules($section) !!}
 			@endcomponent
 		@endcomponent
-	@endfor
-
-	@var($section = 'copyright')
-	@component('template::front.section', compact('section'))
-		@var($width = $this->containerWidth($section))
-		@component('template::front.container', compact('section', 'width')) 
-			{!! $this->renderedModules($section) !!}
-		@endcomponent
-	@endcomponent
+	@endif
 </div> 
